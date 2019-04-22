cf3e78107dc03ac561fa67735605351fab5c0440
ManuelJimenez27 <47027906+ManuelJimenez27@users.noreply.github.com>
2019-04-22 13:01:15 +0200

    Add files via upload

---
+++ T7EJ1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package t7ej1;
+
+/**
+ *
+ * @author manuel
+ */
+public class T7EJ1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    char[] num = new char[12];
+    num[0]='9';
+    num[1]='2';
+    num[4]='0';
+    num[6]='4';
+    num[8]='5';
+    num[9]='0';
+    
+        System.out.println(num);
+    
+    }
+    
+}
---
+++ T7EJ9.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package t7ej9;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author manuel
+ */
+public class T7EJ9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca 8 numeros de uno en uno");
+        int[] num = new int[8];
+        for (int i = 0; i < 8; i++) {
+            num[i] = sc.nextInt();
+        if (num[i] % 2 == 0) {
+            System.out.println(num[i] + " par");
+        } else {
+            System.out.println(num[i] + " impar");
+        }
+        }
+       
+    }
+
+}
